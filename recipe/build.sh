fc83075b94b7594459906f6e9e4d5f0c3e862f47
Duncan Macleod <macleoddm@cardiff.ac.uk>
2026-08-26 11:24:55 +0100

    chore: housekeeping
    Build against oldest compatible version of Fd

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,14 +4,12 @@ mkdir -p _build
 pushd _build
 
 # configure
-cmake ${SRC_DIR} \
-        ${CMAKE_ARGS} \
-	-DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
-	-DCMAKE_INSTALL_DATADIR:PATH=${SRC_DIR}/trash \
-	-DCMAKE_INSTALL_LIBDIR:PATH="lib" \
-	-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-       -DPython3_EXECUTABLE:FILE=${PYTHON} \
-;
+cmake \
+  ${CMAKE_ARGS} \
+  -DCMAKE_INSTALL_DATADIR:PATH=${SRC_DIR}/trash \
+  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+  -DPython3_EXECUTABLE:FILE=${PYTHON} \
+  ${SRC_DIR}
 
 # build
 cmake --build . --parallel ${CPU_COUNT}
@@ -19,5 +17,5 @@ cmake --build . --parallel ${CPU_COUNT}
 # install
 cmake --build . --parallel ${CPU_COUNT} --target install
 
-#test
-ctest
+# test
+ctest --output-on-failure --parallel ${CPU_COUNT} --verbose
